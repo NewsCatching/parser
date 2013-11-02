3554828c2fa115a697cad7e36de5db84484b0731
grassboy <github@grassboy.tw>
2013-11-02 19:40:33 +0800

    api fix

--- parse_api.php
+++ parse_api.php
@@ -7,14 +7,15 @@ input:
   format
 output
   error
-  msg
+  msg 
   guid: md5
+  id
 */
 Class ParseAPI {
   public static function error($msg){
     return array("error"=>1, "msg"=>$msg);
   }
-  public static function request($url, $format="debug"){
+  public static function request($url, $format=""){
     foreach(Parser::$support_url as $pattern=>$parser){
       if(preg_match($pattern, $url)){
         $result = new $parser($url, "");
@@ -27,14 +28,17 @@ Class ParseAPI {
     switch($format){
     case "debug":
       return sprintf("<xmp>%s</xmp>", print_r(array(
-        'msg'=>0,
+        'error'=>0,
+        'msg'=>'',
         'guid'=>md5($url),
         'id'=>$data_index,
         'debug'=>$result->toArray()
       ), true));
+      break;
     default:
       return json_encode(array(
-        'msg'=>0,
+        'error'=>0,
+        'msg'=>'',
         'guid'=>md5($url),
         'id'=>$data_index
       ));
@@ -43,10 +47,11 @@ Class ParseAPI {
   }
 }
 $url =$_GET['url'];
+$format = $_GET['format'];
 if($_SERVER['PHP_SELF'] == "/hackday2013/parse_api.php"){
   if(!$url) $url = "http://blog.xuite.net/grassboy/Tech/49071851-%5BUglyCode%5D+%E5%BF%AB%E9%80%9F%E5%8F%96%E5%BE%97KeyCode%E7%9A%84%E6%96%B9%E6%B3%95";
   exit_r(ParseAPI::request($url, 'debug'));
 } else {
-  echo ParseAPI::request($url);
+  echo ParseAPI::request($url, $format);
 }
 ?>
