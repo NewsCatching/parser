3682db994a261925cfe8076ac269d0792ca23cf5
grassboy <github@grassboy.tw>
2013-11-03 00:10:09 +0800

    ptt parser done

--- parser.php
+++ parser.php
@@ -213,6 +213,45 @@ class PeopoParser extends Parser {
     self::parse();
   }
 } 
+class PttParser extends Parser {
+  public function parse(){
+    $html = $this->raw;
+    phpQuery::newDocumentHTML($html);
+
+    $this->title = $this->og_title = pq('meta[property="og:title"]')->attr("content");
+    $this->og_description = pq('meta[property="og:description"]')->attr("content");
+    $body_img = pq('#main-content img:eq(0)');
+    if($body_img->length > 0){
+      $this->og_image = $body_img->attr("src");
+    } else {
+      $this->og_image = pq('meta[property="og:image"]')->attr("content");
+    }
+    $this->referral = "批踢踢實業坊";
+    $date_dom = pq('#main-content .article-meta-tag:contains("時間"):first');
+    if($date_dom->length == 1){
+      $this->publish_time = date("Y-m-d H:i:s", strtotime());
+    } else {
+      $this->publish_time = date("Y-m-d H:i:s");
+    }
+    $youtube_players = pq('iframe.youtube-player');
+    foreach($youtube_players as $player){
+      $src = pq($player)->attr('src');
+      preg_match('/\/embed\/([^\/]+)$/', $src, $matches);
+      if($matches){
+        $video_id = $matches[1];
+        pq($player)->replaceWith(sprintf("<a href=\"http://youtu.be/%s\" target=\"_blank\" data-video-id=\"%s\" class=\"news-catch-link\"><img src=\"http://i.ytimg.com/vi/%s/1.jpg\"></a>", $video_id, $video_id, $video_id));
+      } else if($src){
+        pq($player)->replaceWith(sprintf("<a href=\"%s\" target=\"_blank\" class=\"news-catch-link\">前往觀看</a>", $src));
+      }
+    }
+    $this->body = pq('#main-content')->html();
+    $this->is_support = 1;
+  }
+  function __construct($url, $rss_referral) {
+    parent::__construct($url, $rss_referral);
+    self::parse();
+  }
+}
 class DefaultParser extends Parser {
   public function parse(){
     $html = $this->raw;
@@ -238,6 +277,7 @@ class DBParser extends Parser {
   }
 } 
 Parser::$support_url = array(
+  '/\/\/www\.ptt\.cc\/bbs\/.*\/M\.[^\/]+\.html/'=>PttParser, //http://www.ptt.cc/bbs/asciiart/M.1383145289.A.4F8.html
   '/\/\/tw\.news\.yahoo\.com\/[^\/]+\.html/'=>YahooParser,
   '/\/\/www\.peopo\.org\/news\//'=>PeopoParser
 );
--- parser_test.php
+++ parser_test.php
@@ -1,10 +1,50 @@
 <?
 include('parser.php');
+class PttParser extends Parser {
+  public function parse(){
+    $html = $this->raw;
+    phpQuery::newDocumentHTML($html);
+
+    $this->title = $this->og_title = pq('meta[property="og:title"]')->attr("content");
+    $this->og_description = pq('meta[property="og:description"]')->attr("content");
+    $body_img = pq('#main-content img:eq(0)');
+    if($body_img->length > 0){
+      $this->og_image = $body_img->attr("src");
+    } else {
+      $this->og_image = pq('meta[property="og:image"]')->attr("content");
+    }
+    $this->referral = "批踢踢實業坊";
+    $date_dom = pq('#main-content .article-meta-tag:contains("時間"):first');
+    if($date_dom->length == 1){
+      $this->publish_time = date("Y-m-d H:i:s", strtotime());
+    } else {
+      $this->publish_time = date("Y-m-d H:i:s");
+    }
+    $youtube_players = pq('iframe.youtube-player');
+    foreach($youtube_players as $player){
+      $src = pq($player)->attr('src');
+      preg_match('/\/embed\/([^\/]+)$/', $src, $matches);
+      if($matches){
+        $video_id = $matches[1];
+        pq($player)->replaceWith(sprintf("<a href=\"http://youtu.be/%s\" target=\"_blank\" data-video-id=\"%s\" class=\"news-catch-link\"><img src=\"http://i.ytimg.com/vi/%s/1.jpg\"></a>", $video_id, $video_id, $video_id));
+      } else if($src){
+        pq($player)->replaceWith(sprintf("<a href=\"%s\" target=\"_blank\" class=\"news-catch-link\">前往觀看</a>", $src));
+      }
+    }
+    $this->body = pq('#main-content')->html();
+    $this->is_support = 1;
+  }
+  function __construct($url, $rss_referral) {
+    parent::__construct($url, $rss_referral);
+    self::parse();
+  }
+}
 if($_SERVER['PHP_SELF'] == "/hackday2013/parser_test.php"){
-  $db_item = new DBParser(
-    "http://tw.news.yahoo.com/nba--%E7%90%83%E9%9E%8B%E7%BE%A9%E8%B3%A3%E6%8B%8D%E5%BE%9777%E8%90%AC-%E6%9E%97%E6%9B%B8%E8%B1%AA%E9%8C%84%E5%BD%B1%E8%AC%9D%E8%B2%B7%E4%B8%BB-070009566"
+  $db_item = new PttParser(
+    "http://www.ptt.cc/bbs/asciiart/M.1383145289.A.4F8.html",
+    ""
   );
-  $db_item->toDB();
+  //$db_item->toDB();
   exit_r($db_item->toString());
 }
 ?>
